4b7b388ce7fea39e0e19b4479b132adb9f5c1a15
yize.li <yize.li@wustl.edu>
2019-05-07 17:55:04 -0500

    Configuration file which needs to be modified by the users

--- config.sh
+++ config.sh
@@ -1,11 +1,11 @@
-
-  input_sample_name=" " # Sample name
-  input_ref=" " # Path saving the reference (e.g. path/name, no need to include the *.fa file name) 
-  input_fastq=" " # Path saving the FASTQ files (e.g. path/name, no need to include the *.fastq.gz file name)
-  input_datatype=" " # WES or WGS
-  input_build="GRCh38" # Reference built
-  input_target_bed="/diskmnt/Projects/Users/yize.li/Reference/S31285117_hs_hg38/S31285117_Regions.bed"
-  input_CNV_bed="/diskmnt/Projects/Users/yize.li/PKD/Reference/agilent_v6r2_cnvfilter.bed"
-  working_path=$(pwd -P) # Current working path
-  input_gene_interest=${working_path}/Gene_interest.txt
-  vep_cache_path="/diskmnt/Datasets/VEP"
+## Arguments
+input_sample_name=" " # Sample name
+input_ref=" " # Path saving the reference (e.g. path/name, no need to include the *.fa file name) 
+input_fastq=" " # Path saving the FASTQ files (e.g. path/name, no need to include the *.fastq.gz file name)
+input_datatype=" " # WES or WGS
+input_build="GRCh38" # Reference built GRCh38 or GRCh37
+input_target_bed=" " # Target BED files. Users can choose to use their own BED or use the one provided in LR-PKD source
+input_CNV_bed=" " # CNV BED files. Users can choose to use their own BED or use the one provided in LR-PKD source
+working_path=$(pwd -P) # Current working path
+input_gene_interest=${working_path}/Gene_interest.txt # Users can modified the list of genes with interest
+vep_cache_path=" " # The path of VEP cache 
