e12de713dbfe0b76170c2351a3ea085a57dccf2b
yize.li <yize.li@wustl.edu>
2019-05-08 12:26:55 -0500

    Configuration file needs to be set by the users before running LR-PKD

--- config.sh
+++ config.sh
@@ -8,4 +8,8 @@ input_target_bed=" " # Target BED files. Users can choose to use their own BED o
 input_CNV_bed=" " # CNV BED files. Users can choose to use their own BED or use the one provided in LR-PKD source
 working_path=$(pwd -P) # Current working path
 input_gene_interest=${working_path}/Gene_interest.txt # Users can modified the list of genes with interest
-vep_cache_path=" " # The path of VEP cache 
+vep_cache_path=" " # The path of VEP cache
+
+### In order to directly call the tools, make sure the paths are saved in ./~bashrc. For instance,
+### export PATH="/path/longranger-2.2.2/longranger-cs/2.2.2/bin:$PATH"
+### export PATH="/path/ensembl-vep/vep:$PATH" 
