56e355e1d1cecf052923ee1691073f81988ddc8e
yize.li <yize.li@wustl.edu>
2019-05-12 19:26:11 -0500

    Dependent script in LR-PKD

--- final_report.pl
+++ final_report.pl
@@ -3,7 +3,7 @@
 open(IN, "$ARGV[0].results");
 open(DA, "$ARGV[0].readcount");
 open(OUT, ">$ARGV[0].output.tsv");
-print OUT "Hugo_Symbol\tNCBI_Build\tChr\tStart\tEnd\tEffect\tType\tRef\tAlt\tNovelty\tNucleotide\tAminoAcid\tGene\tFeature\tFeature_type\tSIFT\tPolyPhen\tCLIN_SIG\tTotal_count\tRef_count\tAlt_count\tVAF\n";
+print OUT "Hugo_Symbol\tNCBI_Build\tChr\tStart\tEnd\tEffect\tType\tRef\tAlt\tNovelty\tNucleotide\tAminoAcid\tGene\tFeature\tFeature_type\tSIFT\tPolyPhen\tCLIN_SIG\tLONGRANGER_FILTER\tGT\tPS\tBX\tTotal_count\tRef_count\tAlt_count\tVAF\tLRPKD_FILTER\n";
 
 while(<IN>)
 {
@@ -39,5 +39,10 @@ while(<IN>)
 		}
 	}
 	$vaf=($nref eq "NA" || $nalt eq "NA") ? "NA" : sprintf("%.3f", $nalt/($m[3]))*100;
-	print OUT "$_\t$m[3]\t$nref\t$nalt\t$vaf\%\n";
+	if (index($l[19], "/") != -1 || $l[20] eq -1 || $nref eq "NA" || $nalt eq "NA" || $vaf < 20 || length(split(';', $l[21])) < 2) {
+		$LRPKD_FILTER = "NOT_PASS"
+	} else {
+		$LRPKD_FILTER = "PASS"
+	}
+	print OUT "$_\t$m[3]\t$nref\t$nalt\t$vaf\%\t$LRPKD_FILTER\n";
 }
