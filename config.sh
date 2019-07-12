35e8b0d97066e88663551deb8ac09f8b796c6470
yize.li <yize.li@wustl.edu>
2019-07-12 11:55:36 -0500

    Configuration file updated by the users before running LR-PKD

--- config.sh
+++ config.sh
@@ -4,11 +4,12 @@ input_ref=" " # Path saving the reference (Users should follow the reference fol
 input_fastq=" " # Path saving the FASTQ files (Users should follow the data folder structure used in Long Ranger. If /sample/1.fastq.gz, set /sample here)
 input_datatype=" " # WES or WGS
 input_build=" " # Reference built GRCh38 or GRCh37
-input_target_bed=" " # Target BED files. Users can choose to use their own BED or use the one provided in LR-PKD source
-input_CNV_bed=" " # CNV BED files. Users can choose to use their own BED or use the one provided in LR-PKD source
+input_target_bed=" " # Target BED files required by WES longranger targeted. Users can choose to use their own BED or use the one provided in LR-PKD source
+input_CNV_bed=" " # CNV BED files required by WES longranger targeted. Users can choose to use their own BED or use the one provided in LR-PKD source
 working_path=$(pwd -P) # Current working path
 input_gene_interest=${working_path}/Gene_interest.txt # Users can modified the list of genes with interest
 vep_cache_path=" " # The path of VEP cache
+gatk_path="" # Required by Long Ranger if users choose to select GATK for variant calling. Such as ./gatk-4.0.3.0/gatk-package-4.0.3.0-local.jar
 
 ### In order to directly call the tools, make sure the paths are saved in ./~bashrc. For instance,
 ### export PATH="/path/longranger-2.2.2/longranger-cs/2.2.2/bin:$PATH"
